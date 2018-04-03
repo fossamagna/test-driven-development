52e38bdfa8f794ee1512344ab9ea9b604dc19758
MURAKAMI Masahiko <fossamagna2@gmail.com>
2018-04-03 22:49:46 +0900

    JUnit5にした

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -1,9 +1,9 @@
 package money;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class MoneyTest {
   @Test
