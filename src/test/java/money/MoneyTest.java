761f24e54107cd9f014e00de0d7526c91e588844
MURAKAMI Masahiko <fossamagna2@gmail.com>
2018-04-03 22:02:41 +0900

    1章完了

---
+++ src/test/java/money/MoneyTest.java
@@ -0,0 +1,15 @@
+package money;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MoneyTest {
+  @Test
+  public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    five.times(2);
+    assertThat(five.amount, is(10));
+  }
+}
